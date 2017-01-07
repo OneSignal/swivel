5c9a45f0ab3199488e3439fe2d2d09ead9dd1039
Jason Pang <jason@jasonpang.me>
2017-01-07 15:05:38 -0800

    Support being loaded in node environment without breaking

--- swivel.js
+++ swivel.js
@@ -1,25 +1,28 @@
 'use strict';
+if (typeof navigator === "undefined") {
 
-var page = require('./page');
-var worker = require('./worker');
-var api;
-
-if ('serviceWorker' in navigator) {
-  api = page();
-} else if ('clients' in self) {
-  api = worker();
 } else {
-  api = {
-    on: complain,
-    once: complain,
-    off: complain,
-    emit: complain,
-    broadcast: complain
-  };
-}
+  var page = require('./page');
+  var worker = require('./worker');
+  var api;
 
-function complain () {
-  throw new Error('Swivel couldn\'t detect ServiceWorker support. Please feature detect before using Swivel in your web pages!');
-}
+  if ('serviceWorker' in navigator) {
+    api = page();
+  } else if ('clients' in self) {
+    api = worker();
+  } else {
+    api = {
+      on: complain,
+      once: complain,
+      off: complain,
+      emit: complain,
+      broadcast: complain
+    };
+  }
 
-module.exports = api;
+  function complain () {
+    throw new Error('Swivel couldn\'t detect ServiceWorker support. Please feature detect before using Swivel in your web pages!');
+  }
+
+  module.exports = api;
+}
