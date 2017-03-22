12ebb9e0a8bcba24ab3ca268b003b2e9833a37e8
Jason Pang <jason@jasonpang.me>
2017-03-22 13:42:55 -0700

    Load unimplemented stub for unsupported environments
    Instead of loading a blank object { }, load an unimplemented stub of the
    methods. This makes it easier to stub out the methods if they already
    exist.

--- swivel.js
+++ swivel.js
@@ -1,28 +1,23 @@
 'use strict';
-if (typeof navigator === "undefined") {
 
-} else {
-  var page = require('./page');
-  var worker = require('./worker');
-  var api;
-
-  if ('serviceWorker' in navigator) {
-    api = page();
-  } else if ('clients' in self) {
-    api = worker();
-  } else {
-    api = {
-      on: complain,
-      once: complain,
-      off: complain,
-      emit: complain,
-      broadcast: complain
-    };
-  }
-
-  function complain () {
-    throw new Error('Swivel couldn\'t detect ServiceWorker support. Please feature detect before using Swivel in your web pages!');
-  }
-
-  module.exports = api;
+function complain() {
+  throw new Error('Swivel couldn\'t detect ServiceWorker support. Please feature detect before using Swivel in your web pages!');
 }
+
+var api = {
+  on: complain,
+  once: complain,
+  off: complain,
+  emit: complain,
+  broadcast: complain
+};
+
+if (typeof navigator !== "undefined") {
+  if ('serviceWorker' in navigator) {
+    api = require('./page');
+  } else if ('clients' in self) {
+    api = require('./worker');
+  }
+}
+
+module.exports = api;
